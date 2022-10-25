45c4eefe252c7d89bdae02523e40bd6fea723f3c
m7real <masayeedreal@gmail.com>
2022-10-25 14:28:30 +0600

    router added

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthProvider = ({ children }) => {
+  return (
+    <div>
+      <h2>I am authProvider compo</h2>
+    </div>
+  );
+};
+
+export default AuthProvider;
