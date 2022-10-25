60277625c96bface7729fadb7bb31df83d469735
m7real <masayeedreal@gmail.com>
2022-10-25 19:26:47 +0600

    navbar updated

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import logo from "../../../assets/logo/e-logo.png";
 
 const Header = () => {
+  const [checked, setChecked] = useState(true);
+
   return (
     <div className="navbar bg-primary text-primary-content">
       <div className="navbar-start">
@@ -15,57 +18,38 @@ const Header = () => {
             <li>
               <Link>Item 1</Link>
             </li>
-            <li tabIndex={0}>
-              <Link className="justify-between">
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </Link>
-              <ul className="p-2">
-                <li>
-                  <Link>Submenu 1</Link>
-                </li>
-                <li>
-                  <Link>Submenu 2</Link>
-                </li>
-              </ul>
-            </li>
             <li>
               <Link>Item 3</Link>
             </li>
           </ul>
         </div>
-        <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+        {/* <a href="https://www.flaticon.com/free-icons/letter-e" title="letter e icons">Letter e icons created by Hight Quality Icons - Flaticon</a> */}
+        <img className="w-8" src={logo} alt="" />
+        <Link className="btn btn-ghost normal-case text-xl">Easy Learn</Link>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal p-0">
-          <li>
-            <Link>Item 1</Link>
-          </li>
-          <li tabIndex={0}>
-            <Link>
-              Parent
-              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </Link>
-            <ul className="p-2">
-              <li>
-                <Link>Submenu 1</Link>
-              </li>
-              <li>
-                <Link>Submenu 2</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link>Item 3</Link>
-          </li>
-        </ul>
-      </div>
+
       <div className="navbar-end">
-        <Link className="btn">Get started</Link>
+        <div className="hidden lg:flex ">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <Link>Courses</Link>
+            </li>
+
+            <li>
+              <Link>FAQ</Link>
+            </li>
+            <li>
+              <Link>Blog</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="form-control mx-3">
+          <label className="label cursor-pointer">
+            <span className="label-text mx-1">{checked ? "Dark" : "Light"}</span>
+            <input onClick={() => setChecked(!checked)} type="checkbox" className="toggle toggle-sm ml-1 mr-4" checked={checked} />
+          </label>
+        </div>
+        <Link className="btn">Log In</Link>
       </div>
     </div>
   );
