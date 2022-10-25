b166df8ec7fb40d3141d1c1dc5164e013045d71f
m7real <masayeedreal@gmail.com>
2022-10-26 04:17:42 +0600

    course detail added

--- src/Pages/Courses/Course/Course.js
+++ src/Pages/Courses/Course/Course.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Course = ({ course }) => {
-  const { _id, title, price, img, details, rating } = course;
+  const { _id, title, price, img, rating } = course;
   const navigate = useNavigate();
 
   const handleNavigate = () => {
--- src/Pages/Courses/CourseDetail/CourseDetail.js
+++ src/Pages/Courses/CourseDetail/CourseDetail.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const CourseDetail = () => {
+  const course = useLoaderData();
+  const { _id, title, price, img, details, rating } = course;
   return (
-    <div>
-      <h2>Course Detail Here</h2>
+    <div className="w-9/12 mx-auto  mt-12">
+      <h2 className="text-4xl font-semibold mb-6 text-center">{title}</h2>
+      <hr />
+      <img className="mx-auto my-6 w-96" src={img} alt="" />
+
+      <p>{details}</p>
+      <p className="font-bold">Price: ${price}</p>
+      <p className="font-bold">Rating: {rating}</p>
     </div>
   );
 };
