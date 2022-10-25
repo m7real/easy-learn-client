b34bd1bb2515701bac9ac455e1a3ba153bfd82b1
m7real <masayeedreal@gmail.com>
2022-10-26 03:07:11 +0600

    course cards added

--- src/Pages/Courses/Course/Course.js
+++ src/Pages/Courses/Course/Course.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Course = () => {
+const Course = ({ course }) => {
+  const { _id, title, price, img, details, rating } = course;
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(`/course/${_id}`);
+  };
+
   return (
-    <div>
-      <h2>Course Card</h2>
+    <div onClick={handleNavigate} className="card w-full lg:w-80 mx-auto bg-base-300 shadow-xl hover:cursor-pointer">
+      <figure className="px-10 pt-10">
+        <img src={img} alt="" className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{title}</h2>
+        <div className="flex justify-around w-full my-4">
+          <small>Price: ${price}</small>
+          <small>Rating: {rating}</small>
+        </div>
+        <div className="card-actions">
+          <button className="btn btn-primary btn-sm btn-wide">See Details</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Courses/Courses/Courses.js
+++ src/Pages/Courses/Courses/Courses.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import Course from "../Course/Course";
 
 const Courses = () => {
   const courses = useLoaderData();
@@ -8,14 +9,18 @@ const Courses = () => {
     <div className="grid lg:grid-cols-12 text-slate-200 gap-5 w-10/12 mx-auto mt-10">
       {/* Left Side Nav */}
       <div className=" lg:col-span-3 bg-base-300 shadow-xl rounded-lg  min-h-fit p-4">
-        {courses.map((c) => (
-          <p key={c._id} className="bg-base-100 mt-3 px-4 py-3 rounded-lg hover:text-slate-400">
-            <Link to={`/course/${c._id}`}>{c.title}</Link>
+        {courses.map((course) => (
+          <p key={course._id} className="bg-base-100 mt-3 px-4 py-3 rounded-lg hover:text-slate-400">
+            <Link to={`/course/${course._id}`}>{course.title}</Link>
           </p>
         ))}
       </div>
       {/* Right Side Card Section */}
-      <div className="lg:col-span-9 bg-base-300 min-h-screen"></div>
+      <div className="lg:col-span-9 rounded-lg grid lg:grid-cols-2 gap-x-3 gap-y-8">
+        {courses.map((course) => (
+          <Course key={course._id} course={course}></Course>
+        ))}
+      </div>
     </div>
   );
 };
